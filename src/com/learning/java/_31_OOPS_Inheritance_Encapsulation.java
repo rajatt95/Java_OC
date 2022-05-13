e15d3d95c1b6a76bd385607cb8a30b7878e90a65
“rajatt95” <rajatvermaa95@gmail.com>
2022-05-14 02:25:20 +0530

    UDEMY - OC - Java (OOPS - Encapsulation completed)

---
+++ src/com/learning/java/_31_OOPS_Inheritance_Encapsulation.java
@@ -0,0 +1,31 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Selenium Java Test Framework & Best Practices - Masterclass (https://www.udemy.com/course/selenium-java-test-framework/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+/***************************************************/
+
+package com.learning.java;
+
+public class _31_OOPS_Inheritance_Encapsulation {
+
+    // Encapsulation:
+        // Wrapping up of data under a single unit! The data is protected!
+
+    // Field - Private
+    private String str ="Private String";
+
+    // Method - Public
+    public String getPrivateString(){
+        return str;
+    }
+}// class
+
+
+
+
+
---
+++ src/com/learning/java/_32_OOPS_Inheritance_Encapsulation_App.java
@@ -0,0 +1,25 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: Selenium Java Test Framework & Best Practices - Masterclass (https://www.udemy.com/course/selenium-java-test-framework/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+/***************************************************/
+
+package com.learning.java;
+
+public class _32_OOPS_Inheritance_Encapsulation_App {
+
+    public static void main(String[] args) {
+        _31_OOPS_Inheritance_Encapsulation obj = new _31_OOPS_Inheritance_Encapsulation();
+        System.out.println("obj.getPrivateString() = " + obj.getPrivateString()); // Private String
+    }// main
+}// class
+
+
+
+
+
